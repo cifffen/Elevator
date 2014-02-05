869d40fc9a798238eff55b146313fac6dd048c51
cifffen <christianfsaetre@gmail.com>
2014-02-05 09:55:06 +0100

    Cgo drivers test
    Calling the c-flies from go

--- Drivers/elevdrivers.go
+++ Drivers/elevdrivers.go
@@ -10,28 +10,43 @@ import "C"
 type TagElevLampType int
 
 const(
-	ButtonCallUp TagElevLamType = iota
+	ButtonCallUp TagElevLampType = iota
 	ButtonCallDown
 	ButtonCommand
 )
 
+func ElevInit() int{
+	return int(C.elev_init(void))
+}
 
-int elev_init(void);
+func ElevSetSpeed(speed int){
+	C.elev_set_speed(speed)
+}
 
-void elev_set_speed(int speed);
+func ElevGetFloorSensorSignal() int{
+	return int(C.elev_get_floor_sensor_signal())
+}
 
-int elev_get_floor_sensor_signal(void);
+func ElevGetButtonSignal(button TagElevLampType, floor int) int{
+	return int(C.elev_get_button_signal(button,floor))
+}
 
-int elev_get_button_signal(elev_button_type_t button, int floor);
+func ElevGetStopSignal() int{
+	return int(C.elev_get_stop_signal())
+}
 
-int elev_get_stop_signal(void);
+func ElevSetFloorIndicator(floor int){
+	C.elev_set_floor_indicator(floor)
+}
 
-int elev_get_obstruction_signal(void);
+func ElevSetButtonLamp(button TagElevLampType, floor int, value int){
+	C.elev_set_button_lamp(button,floor,value)
+}
 
-void elev_set_floor_indicator(int floor);
+func ElevSetStopLamp(value int){
+	C.elev_set_stop_lamp(value)
+}
 
-void elev_set_button_lamp(elev_button_type_t button, int floor, int value);
-
-void elev_set_stop_lamp(int value);
-
-void elev_set_door_open_lamp(int value);
+func ElevSetDoorOpenLamp(value int){
+	C.void elev_set_door_open_lamp(value)
+}
