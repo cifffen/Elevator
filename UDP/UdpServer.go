c47fa261ca4ec0aae6dd793a529ca9c53da161f8
cifffen <christianfsaetre@gmail.com>
2014-02-19 19:39:19 +0100

    Oppdatert UDP

--- UDP/UdpServer.go
+++ UDP/UdpServer.go
@@ -21,33 +21,52 @@ type ButtonMessage struct {
 }
 
 func send(msg ButtonMessage)(){
+	addr, _ := net.ResolveUDPAddr("udp", "192.168.1.255:2224")
 	buf,err1 := json.Marshal(msg)
 	if err1 != nil{
 		fmt.Println(err1)
 	}
 	//rAddr, _ := net.ResolveUDPAddr("udp", "192.168.1.255:2224")
-	sock.Write(buf)
+	_,err1 =sock.WriteTo(buf,addr)
+	if err1 != nil{
+		fmt.Println(err1)
+	}
+	fmt.Printf("her \n")
 }
 func main() {
 	var m ButtonMessage
 	addr, _ := net.ResolveUDPAddr("udp", ":2224")
 	sock, _ = net.ListenUDP("udp", addr)
 	fmt.Println("Connected")
+	m1 :=ButtonMessage{
+		Floor: 5,
+		Button: 2,
+		Action: Tender,
+	}
+	send(m1)
+	i:=0
 	for {
+		if i >=10{
+			break
+		}
+		i++
 		buf := make([]byte, 1024)
 		rlen, address, err := sock.ReadFromUDP(buf)
-		err = json.Unmarshal(buf[0:rlen], &m)
-		fmt.Println(address)
-		if err != nil {
-		  fmt.Println(err)
+		sAddr, _ := net.ResolveUDPAddr("udp", "localhost:2224")
+		if address != sAddr{
+			err = json.Unmarshal(buf[0:rlen], &m)
+			fmt.Println(address)
+			if err != nil {
+			  fmt.Println(err)
+			}
+			fmt.Println(buf[0:rlen])
+			//send(m)
+			//_, err = sock.WriteToUDP(buf,address)
+			if err != nil{
+				fmt.Println(err)
+			}
+			fmt.Printf("Floor: %d, Button: %d\n, Action: %d", m.Floor, m.Button, m.Action)
+			//go handlePacket(buf, rlen)
 		}
-		fmt.Println(buf[0:rlen])
-		send(m)
-		//_, err = sock.WriteToUDP(buf,address)
-		if err != nil{
-			fmt.Println(err)
-		}
-		fmt.Printf("Floor: %d, Button: %d\n, Action: %d", m.Floor, m.Button, m.Action)
-		//go handlePacket(buf, rlen)
 	}
 }
