4c554203feaa4721bdd1db6ef76386976d3eb7c2
cifffen <christianfsaetre@gmail.com>
2014-02-19 15:09:39 +0100

    UDP oppdatering

--- UDP/UdpClient.go
+++ UDP/UdpClient.go
@@ -2,21 +2,56 @@ package main
 import (
   "net"
   "fmt"
+  "encoding/json"
+
+)
+type ActionType int
+
+const(
+	NewOrder ActionType = iota
+	DeleteOrder
+	Tender
 )
 
+type ButtonMessage struct {
+	Floor int
+	Button int
+	Action ActionType
+}
+
 func main() {
-  con, _ := net.Dial("udp", "192.168.1.255:2224")
-  buf := []byte("bla bla bla I am the packet\n")
-  _, err := con.Write(buf)
-  if err != nil {
-    fmt.Println(err)
-  }
-  fmt.Println(1) 
-  buf2 := make([]byte, 1024)
-  rlen, err :=con.Read(buf2)
-  fmt.Println(1)
-  if err != nil {
-    fmt.Println(err)
-  }
-  fmt.Println(string(buf[0:rlen]))
+
+	x:=5
+	m :=ButtonMessage{
+		Floor: x,
+		Button: 2,
+		Action: Tender,
+	}
+	buf,err1 := json.Marshal(m)
+	if err1 != nil{
+		fmt.Println(err1)
+	}
+	/*
+	aaaaaaaaa
+	*/
+	addr, _ := net.ResolveUDPAddr("udp", "192.168.1.255:2224")
+	addr2, _ := net.ResolveUDPAddr("udp", "192.168.1.4:2422")
+	con, _ := net.DialUDP("udp", addr2, addr)
+	//buf := []byte("bla bla bla I am the packet\n")
+	_, err := con.Write(buf)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Printf("1") 
+	var m1 ButtonMessage
+	buf2 := make([]byte, 1024)
+	var rlen int
+	rlen,_, err =con.ReadFromUDP(buf2)
+	_ = json.Unmarshal(buf2[0:rlen],&m1)
+	fmt.Printf("1")
+	if err != nil {
+		fmt.Println(err)
+	}
+	
+	fmt.Printf("Floor: %d, Button: %d", m1.Floor, m1.Button)
 }
--- UDP/UdpServer.go
+++ UDP/UdpServer.go
@@ -3,21 +3,51 @@ package main
 import (
   "net"
   "fmt"
+  "encoding/json"
+)
+var sock *net.UDPConn
+type ActionType int
+
+const(
+	NewOrder ActionType = iota
+	DeleteOrder
+	Tender
 )
 
+type ButtonMessage struct {
+	Floor int
+	Button int
+	Action ActionType
+}
+
+func send(msg ButtonMessage)(){
+	buf,err1 := json.Marshal(msg)
+	if err1 != nil{
+		fmt.Println(err1)
+	}
+	//rAddr, _ := net.ResolveUDPAddr("udp", "192.168.1.255:2224")
+	sock.Write(buf)
+}
 func main() {
-  addr, _ := net.ResolveUDPAddr("udp", ":2224")
-  sock, _ := net.ListenUDP("udp", addr)
-  fmt.Println("Connected")
-  for {
-    buf := make([]byte, 1024)
-    rlen, address, err := sock.ReadFromUDP(buf)
-	fmt.Println(address)
-    if err != nil {
-      fmt.Println(err)
-    }
-	_, err = sock.WriteToUDP(buf,address)
-    fmt.Println(string(buf[0:rlen]))
-    //go handlePacket(buf, rlen)
-  }
+	var m ButtonMessage
+	addr, _ := net.ResolveUDPAddr("udp", ":2224")
+	sock, _ = net.ListenUDP("udp", addr)
+	fmt.Println("Connected")
+	for {
+		buf := make([]byte, 1024)
+		rlen, address, err := sock.ReadFromUDP(buf)
+		err = json.Unmarshal(buf[0:rlen], &m)
+		fmt.Println(address)
+		if err != nil {
+		  fmt.Println(err)
+		}
+		fmt.Println(buf[0:rlen])
+		send(m)
+		//_, err = sock.WriteToUDP(buf,address)
+		if err != nil{
+			fmt.Println(err)
+		}
+		fmt.Printf("Floor: %d, Button: %d\n, Action: %d", m.Floor, m.Button, m.Action)
+		//go handlePacket(buf, rlen)
+	}
 }
